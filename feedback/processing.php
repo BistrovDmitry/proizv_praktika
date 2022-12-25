3a2bdd13accfed379c2b609728469b72d97184d2
ivan <ivan.lyachin@mail.ru>
2022-12-25 23:57:51 +0300

    commit

--- feedback/processing.php
+++ feedback/processing.php
@@ -1,11 +1,10 @@
 <?php
-$to = "zasmolin.e@gmail.com"; // емайл получателя данных из формы
-$tema = "Форма обратной связи на PHP"; // тема полученного емайла
-$message = "Ваше имя: ".$_POST['name']."<br>";//присвоить переменной значение, полученное из формы name=name
-$message .= "E-mail: ".$_POST['email']."<br>"; //полученное из формы name=email
-$message .= "Номер телефона: ".$_POST['phone']."<br>"; //полученное из формы name=phone
-$message .= "Сообщение: ".$_POST['message']."<br>"; //полученное из формы name=message
-$headers = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
-$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
-mail($to, $tema, $message, $headers); //отправляет получателю на емайл значения переменных
+$to = "zasmolin.e@gmail.com";
+$tema = "Форма обратной связи на PHP";
+$message = "Ваше имя: ".$_POST['name']."<br>";
+$message .= "E-mail: ".$_POST['email']."<br>";
+$message .= "Сообщение: ".$_POST['message']."<br>";
+$headers = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+mail($to, $tema, $message, $headers);
 ?>
